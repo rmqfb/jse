4a01c7b629de511c13e61dc4e247a58c81d051df
취업반_PM <_PM@ITbank011-PC>
2015-05-21 20:57:39 +0900

    2015.5.21

--- jse/src/oop05/polymorphism/WebService.java
+++ jse/src/oop05/polymorphism/WebService.java
@@ -1,6 +1,6 @@
 package oop05.polymorphism;
 
-public interface WebService extends WebDao{  // 인터페이스 끼리 상속 가능
+public interface WebService extends WebDao{  // 인터페이스 끼리 상속 가능 but 권유하지 않는 방법이다.
 	public String getAge();
 	public String setAge();
 	public String updateAge();
